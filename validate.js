650e9e90f0a3d4858449e07accc10791587c4133
ellarekow <ellarekow@gmail.com>
2022-03-08 14:38:35 -0600

    Page validation

--- validate.js
+++ validate.js
@@ -1,9 +1,25 @@
 function validate() {
-    const results = check(document.forms["genorator"]["numChps"].value);
+    const chpResults = check(document.forms["gen"]["numChps"].value);
 
+    getNotification(Boolean(chpResults), "numChps");
+    
+    if (chpResults)
+        location.href = "./quiz.html";
 
 }
 
-function check() {
-    return address.match(/^[0-9]+$/);
+function check(name) {
+    return name.match(/^[0-9]+$/);
+}
+
+function getNotification(bool, id) {
+    var label = document.getElementById("labelNotify" + id);
+    if (label == null) {
+        label = document.createElement("LABEL");
+        label.id = "labelNotify" + id;
+        label.setAttribute('class', 'error');
+
+        label.innerHTML = "Please enter a valid number of chapters";
+        document.getElementById(id).appendChild(label);
+    }
 }
