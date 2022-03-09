8a221a701ff7a92efe46f559d37c80000100a810
ellarekow <ellarekow@gmail.com>
2022-03-08 21:54:49 -0600

    Updates on trying to gen the num of probs for chps

--- validate.js
+++ validate.js
@@ -2,7 +2,7 @@ function validate() {
     const chpResults = check(document.forms["gen"]["numChps"].value);
 
     getNotification(Boolean(chpResults), "numChps");
-    
+
     if (chpResults)
         location.href = "./quiz.html";
 
@@ -22,4 +22,20 @@ function getNotification(bool, id) {
         label.innerHTML = "Please enter a valid number of chapters";
         document.getElementById(id).appendChild(label);
     }
+}
+
+function probIntake() {
+    let chps = document.forms["gen"]["numChps"].value;
+
+    for (let i = 0; i < chps; i++) {
+        let textbox = document.getElementById("textArea" + i + 1);
+        if (textbox == null) {
+            textbox = document.createElement("input");
+            textbox.id = "textArea" + i + 1;
+
+            textbox.setAttribute("type", "number");
+            document.getElementById("textArea" + i + 1).appendChild(textbox);
+        }
+    }
+
 }
